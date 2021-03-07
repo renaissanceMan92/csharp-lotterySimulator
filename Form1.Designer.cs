510da32dfbc601328a1c0a16be105e92c6aca199
Robin Holm <49246098+renaissanceMan92@users.noreply.github.com>
2021-03-07 08:09:19 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Lab1Lottery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayBtn = new System.Windows.Forms.Button();
+            this.fiveMatchLabel = new System.Windows.Forms.Label();
+            this.sixMatchLabel = new System.Windows.Forms.Label();
+            this.sevenMatchLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pickNrLabel = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.pickDraws = new System.Windows.Forms.Label();
+            this.drawsTxtBox = new System.Windows.Forms.TextBox();
+            this.sevenMatchTxtBox = new System.Windows.Forms.TextBox();
+            this.sixMatchTxtBox = new System.Windows.Forms.TextBox();
+            this.fiveMatchTxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.Location = new System.Drawing.Point(75, 144);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(56, 23);
+            this.PlayBtn.TabIndex = 0;
+            this.PlayBtn.Text = "PLAY";
+            this.PlayBtn.UseVisualStyleBackColor = true;
+            this.PlayBtn.Click += new System.EventHandler(this.PlayBtn_Click);
+            // 
+            // fiveMatchLabel
+            // 
+            this.fiveMatchLabel.AutoSize = true;
+            this.fiveMatchLabel.Location = new System.Drawing.Point(293, 154);
+            this.fiveMatchLabel.Name = "fiveMatchLabel";
+            this.fiveMatchLabel.Size = new System.Drawing.Size(34, 13);
+            this.fiveMatchLabel.TabIndex = 1;
+            this.fiveMatchLabel.Text = "5 rätt:";
+            // 
+            // sixMatchLabel
+            // 
+            this.sixMatchLabel.AutoSize = true;
+            this.sixMatchLabel.Location = new System.Drawing.Point(293, 200);
+            this.sixMatchLabel.Name = "sixMatchLabel";
+            this.sixMatchLabel.Size = new System.Drawing.Size(34, 13);
+            this.sixMatchLabel.TabIndex = 2;
+            this.sixMatchLabel.Text = "6 rätt:";
+            // 
+            // sevenMatchLabel
+            // 
+            this.sevenMatchLabel.AutoSize = true;
+            this.sevenMatchLabel.Location = new System.Drawing.Point(293, 249);
+            this.sevenMatchLabel.Name = "sevenMatchLabel";
+            this.sevenMatchLabel.Size = new System.Drawing.Size(34, 13);
+            this.sevenMatchLabel.TabIndex = 3;
+            this.sevenMatchLabel.Text = "7 rätt:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(33, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // pickNrLabel
+            // 
+            this.pickNrLabel.AutoSize = true;
+            this.pickNrLabel.Location = new System.Drawing.Point(23, 30);
+            this.pickNrLabel.Name = "pickNrLabel";
+            this.pickNrLabel.Size = new System.Drawing.Size(125, 13);
+            this.pickNrLabel.TabIndex = 5;
+            this.pickNrLabel.Text = "Pick your lucky numbers:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(33, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(233, 27);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(33, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(272, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(33, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(311, 27);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(33, 20);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(350, 27);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(33, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(389, 27);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(33, 20);
+            this.textBox7.TabIndex = 11;
+            // 
+            // pickDraws
+            // 
+            this.pickDraws.AutoSize = true;
+            this.pickDraws.Location = new System.Drawing.Point(23, 78);
+            this.pickDraws.Name = "pickDraws";
+            this.pickDraws.Size = new System.Drawing.Size(145, 13);
+            this.pickDraws.TabIndex = 12;
+            this.pickDraws.Text = "Choose the number of draws:";
+            // 
+            // drawsTxtBox
+            // 
+            this.drawsTxtBox.Location = new System.Drawing.Point(174, 75);
+            this.drawsTxtBox.Name = "drawsTxtBox";
+            this.drawsTxtBox.Size = new System.Drawing.Size(88, 20);
+            this.drawsTxtBox.TabIndex = 13;
+            // 
+            // sevenMatchTxtBox
+            // 
+            this.sevenMatchTxtBox.Location = new System.Drawing.Point(333, 246);
+            this.sevenMatchTxtBox.Name = "sevenMatchTxtBox";
+            this.sevenMatchTxtBox.Size = new System.Drawing.Size(58, 20);
+            this.sevenMatchTxtBox.TabIndex = 14;
+            // 
+            // sixMatchTxtBox
+            // 
+            this.sixMatchTxtBox.Location = new System.Drawing.Point(333, 197);
+            this.sixMatchTxtBox.Name = "sixMatchTxtBox";
+            this.sixMatchTxtBox.Size = new System.Drawing.Size(58, 20);
+            this.sixMatchTxtBox.TabIndex = 15;
+            // 
+            // fiveMatchTxtBox
+            // 
+            this.fiveMatchTxtBox.Location = new System.Drawing.Point(333, 151);
+            this.fiveMatchTxtBox.Name = "fiveMatchTxtBox";
+            this.fiveMatchTxtBox.Size = new System.Drawing.Size(58, 20);
+            this.fiveMatchTxtBox.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 322);
+            this.Controls.Add(this.fiveMatchTxtBox);
+            this.Controls.Add(this.sixMatchTxtBox);
+            this.Controls.Add(this.sevenMatchTxtBox);
+            this.Controls.Add(this.drawsTxtBox);
+            this.Controls.Add(this.pickDraws);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pickNrLabel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sevenMatchLabel);
+            this.Controls.Add(this.sixMatchLabel);
+            this.Controls.Add(this.fiveMatchLabel);
+            this.Controls.Add(this.PlayBtn);
+            this.Name = "Form1";
+            this.Text = "The best lottery app in the multiverse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayBtn;
+        private System.Windows.Forms.Label fiveMatchLabel;
+        private System.Windows.Forms.Label sixMatchLabel;
+        private System.Windows.Forms.Label sevenMatchLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label pickNrLabel;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label pickDraws;
+        private System.Windows.Forms.TextBox drawsTxtBox;
+        private System.Windows.Forms.TextBox sevenMatchTxtBox;
+        private System.Windows.Forms.TextBox sixMatchTxtBox;
+        private System.Windows.Forms.TextBox fiveMatchTxtBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1Lottery
+{
+    public partial class Form1 : Form
+    {
+        /*Klass som implementerar ett lotteri-program som styrs genom ett grafiskt gränssnitt.*/
+        HashSet<int> userNums = new HashSet<int>();
+        int numberOfDraws = 0;
+        bool goodToGo = true;
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlayBtn_Click(object sender, EventArgs e)
+        {
+            /*Metod som anropas när användaren klickar på play.
+             Den anropar i sin tur metoden validateInput() som kollar så att användarens
+             input är giltig. Om den är giltig anropas runLottery(), som då kör
+             själva lotterispelet.*/
+            validateInput();
+            if (goodToGo == false)
+            {
+                MessageBox.Show("No duplicate, missing or invalid numbers!");
+                userNums.Clear();
+                numberOfDraws = 0;
+            }
+            else runLottery();
+        }
+        private void validateInput()
+        {
+            /*Metod som anropas av PlayBtn_Click(). Den lägger till sju lotterisiffror
+             i en hashset, lägger till ett nummer för antal dragning, och kollar så att
+            de har giltiga värden (värda 1-35, är heltal, etc). Om allt är som det ska
+            ges booleanen goodToGo värdet TRUE.*/
+            goodToGo = true;
+            try
+            {
+                userNums.Add(int.Parse(textBox1.Text));
+                userNums.Add(int.Parse(textBox2.Text));
+                userNums.Add(int.Parse(textBox3.Text));
+                userNums.Add(int.Parse(textBox4.Text));
+                userNums.Add(int.Parse(textBox5.Text));
+                userNums.Add(int.Parse(textBox6.Text));
+                userNums.Add(int.Parse(textBox7.Text));
+                numberOfDraws = int.Parse(drawsTxtBox.Text);
+            }
+            catch (Exception)
+            {
+                goodToGo = false;
+            }
+            foreach (int i in userNums)
+            {
+                if (i < 1 || i > 35 || userNums.Count < 7 || numberOfDraws < 1)
+                {
+                    goodToGo = false;
+                }
+            }
+        }
+        private void runLottery()
+        {
+            /*Om boolean goodToGo är TRUE körs denna metod. Den drar sju slumpmässiga siffror,
+             och någon av dessa siffror matchar någon av användarens siffror ökar värdet
+             av variabeln match. I slutet av dragningen kalkyreas hur många matchar som skett.
+             Detta visas sedan i GUIt.*/
+            HashSet<int> lotteryNums = new HashSet<int>();
+            int fivewin = 0;
+            int sixwin = 0;
+            int sevenwin = 0;
+            fiveMatchTxtBox.Text = string.Empty;
+            sixMatchTxtBox.Text = string.Empty;
+            sevenMatchTxtBox.Text = string.Empty;
+            for (int i = 0; i < numberOfDraws; i++)
+            {
+                
+                int match = 0;
+                while (lotteryNums.Count < 7) lotteryNums.Add(rand.Next(1, 35));
+                foreach (int z in lotteryNums)
+                {
+                    foreach (int q in userNums) if (z == q) match++;
+                }
+                if (match == 5) fivewin++;
+                else if (match == 6) sixwin++;
+                else if (match == 7) sevenwin++;
+                lotteryNums.Clear();
+            }
+            fiveMatchTxtBox.AppendText($"{fivewin}");
+            sixMatchTxtBox.AppendText($"{sixwin}");
+            sevenMatchTxtBox.AppendText($"{sevenwin}");
+            userNums.Clear();
+        }
+    }
+}
